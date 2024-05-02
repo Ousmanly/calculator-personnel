2653d76455e96b762ec2ffe2e8adb03ded277d57
Ousmanly <lyousmane492@gmail.com>
2024-05-02 14:47:36 +0200

    Added files

---
+++ calculatrice.js
@@ -0,0 +1,93 @@
+function Multiplication() {
+    document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    
+    // let c = parseFloat(document.getElementById('c').value);
+    
+    if (!isNaN(a) && !isNaN(b) && isNaN(c)) {
+        let resultat = a * b
+        document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;
+        a =document.getElementById('a').value='';
+        b =document.getElementById('b').value='';
+    }
+    // else if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    //     let resultat = a * b * c;
+
+    // document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;
+    // a =document.getElementById('a').value='';
+    // b =document.getElementById('b').value='';
+    // c =document.getElementById('c').value='';
+    // }
+
+
+});
+}
+
+
+
+
+function Division() {
+    document.getElementById("form").addEventListener("submit", function(event) {
+     event.preventDefault();
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    
+    if (!isNaN(a) && !isNaN(b)) {
+        let resultat = a / b
+        document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;
+        a =document.getElementById('a').value='';
+        b =document.getElementById('b').value='';
+     
+    }
+});   
+}
+
+
+   
+function Addition() {
+    document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    
+    // let c = parseFloat(document.getElementById('c').value);
+    
+    if (!isNaN(a) && !isNaN(b) && isNaN(c)) {
+        let resultat = a + b
+        document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;
+        a =document.getElementById('a').value='';
+        b =document.getElementById('b').value='';
+    }
+    
+    // else if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    //     let resultat = a + b + c;
+
+    // document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;
+    // a =document.getElementById('a').value='';
+    // b =document.getElementById('b').value='';
+    // c =document.getElementById('c').value='';
+    // }
+
+   
+}) 
+}
+
+    
+
+function Soustraction() {
+    document.getElementById('form').addEventListener('submit',function(event){
+    event.preventDefault();
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    
+    if (!isNaN(a) && !isNaN(b)) {
+       let resultat = a - b
+       document.getElementById('rs').innerText = 'Le résultat est : ' + resultat;   
+       a =document.getElementById('a').value='';
+       b =document.getElementById('b').value='';
+    }
+})   
+}
+
